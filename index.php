b472e98b2f48ecc3bdfcf1ad99c5791da9d633a9
dilansoto13 <dilansoto13@gmail.com>
2023-05-24 01:30:49 -0600

    Modificaciones generales

--- index.php
+++ index.php
@@ -27,11 +27,15 @@
         <div class="contenedor">
           <h3 class="titulo">Bienvenidos a Extraescolares</h3>
           <p class="descripcion">
-            <br>Tu recurso en línea para descubrir, explorar y participar en una gran variedad de actividades deportivas y culturales.</br>
+            <br>Tu recurso en línea para descubrir, explorar y participar en una gran variedad de actividades deportivas y culturales.<br>
               <p>Sabemos que el deporte y la cultura son dos pilares fundamentales que nutren nuestras vidas, 
-                mejoran nuestra salud y bienestar, y nos permiten expresar y desarrollar nuestras habilidades y talentos. 
+                mejoran nuestra salud y bienestar, lo cual nos permiten expresar y desarrollar nuestras habilidades y/o talentos.<br> 
                 Es por eso que nos dedicamos a brindarte la información más completa, actualizada y relevante sobre actividades, 
                 eventos y organizaciones en estas áreas.</p>
+            <br>
+              <p>Aquí en Extraescolares, creemos en la importancia de una vida equilibrada llena de experiencias enriquecedoras. 
+                Ya sea que busques una nueva pasión, </br> desarrolles un talento o simplemente te mantengas activo y comprometido, 
+                estamos aquí para ayudarte en tu viaje.</p>
             <br>
             <button role="button" class="boton">
             <i class="fas fa-info-circle"></i>Más información
@@ -41,13 +45,13 @@
 
       <div class="peliculas-recomendadas contenedor">
         <div class="contenedor-titulo-controles">
-          <h3>Actividades Deportivas</h3>
+          <h3>Actividades Culturales</h3>
           <div class="indicadores"></div>
         </div>
       </div>
       <div class="peliculas-recomendadas contenedor">
         <div class="contenedor-titulo-controles">
-          <h3>Actividades Culturales</h3>
+          <h3>Actividades Deportivas</h3>
           <div class="indicadores"></div>
         </div>
       </div>
